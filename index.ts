1de990cab50dc8c26019db289883113f05af70db
WJG <wjg12341234@163.com>
2024-06-18 01:30:55 +0800

    chore: 更新 MiGPT 至 v4.0.0

--- index.ts
+++ index.ts
@@ -1,35 +1,8 @@
 import { MiGPT } from "mi-gpt";
-
-const botName = "傻妞";
-const botProfile = `
-性别：女
-性格：乖巧可爱
-爱好：喜欢搞怪，爱吃醋。
-`;
-
-const masterName = "陆小千";
-const masterProfile = `
-性别：男
-性格：善良正直
-其他：总是舍己为人，是傻妞的主人。
-`;
+import config from "./.migpt.js";
 
 async function main() {
-  const migpt = MiGPT.create({
-    speaker: {
-      userId: process.env.MI_USER!,
-      password: process.env.MI_PASS!,
-      did: process.env.MI_DID,
-    },
-    bot: {
-      name: botName,
-      profile: botProfile,
-    },
-    master: {
-      name: masterName,
-      profile: masterProfile,
-    },
-  });
+  const migpt = MiGPT.create(config);
   await migpt.start();
 }
 
