a0f1750b9838490e6e1daad256252d580d0a6045
WJG <wjg12341234@163.com>
2024-06-30 18:34:17 +0800

    refactor: 使用 proxy-agent 替换 https-proxy-agent 和 socks-proxy-agent

--- src/services/proxy.ts
+++ src/services/proxy.ts
@@ -1,11 +1,3 @@
-import { HttpsProxyAgent } from "https-proxy-agent";
-import { SocksProxyAgent } from "socks-proxy-agent";
+import { ProxyAgent } from "proxy-agent";
 
-const httpProxy = process.env.HTTP_PROXY;
-const socksProxy = process.env.SOCKS_PROXY;
-
-export const kProxyAgent = httpProxy
-    ? new HttpsProxyAgent(httpProxy)
-    : socksProxy
-        ? new SocksProxyAgent(socksProxy)
-        : null;
+export const kProxyAgent = new ProxyAgent();
